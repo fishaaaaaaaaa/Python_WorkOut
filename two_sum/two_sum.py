2a007a0346d00a8d88ef81e62b224cd99c90fa82
fishaaaaaaaaa <fihsa@internet.ru>
2022-12-16 17:45:50 +0300

    WorkOut_Five

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -2,9 +2,12 @@ def two_sum(nums, target):
     
     for i in range(len(nums)):
         
-        if nums[i] + nums[i + 1] == target:
+        for j in range(i + 1, len(nums)):
+                
+                if nums[i] + nums[j] == target:
             
-            return [i, i + 1]
+                    return [i, j]
+
 
 print(two_sum([2,7,11,15], 9))
 print(two_sum([3,2,4], 6))
